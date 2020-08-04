c73a6ec14ff9a404482b8ffdca36260072832fea
Thaddeus Metz <temetz90@gmail.com>
2020-08-04 10:22:33 -0500

    branch to store differences between laptop/desktop

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,8 @@
 import React from "react"
 import NavBar from "../../components/NavBar"
-import ContactMe from '../../components/ContactMe'
-import AboutMe from '../../components/AboutMe'
-import Projects from '../../components/Projects'
+import ContactMe from "../../components/ContactMe"
+import AboutMe from "../../components/AboutMe"
+import Projects from "../../components/Projects"
 
 import "bootstrap/dist/css/bootstrap.min.css"
 import "../style.css"
@@ -10,18 +10,15 @@ import "../style.css"
 export default function Home() {
   return (
     <div>
-      <div id='header'>
-
-      {/* <img id="header" src='giphy.gif' alt='placeholder for header'></img> */}
-      <div id='header-text'>
-
-      <h1>Let's get started!</h1>
-      </div>
+      <div id="header">
+        <div id="header-text">
+          <h1>Let's get started!</h1>
+        </div>
       </div>
       <NavBar />
-      <AboutMe/>
-      <Projects/>
-      <ContactMe/>
+      <AboutMe />
+      <Projects />
+      <ContactMe />
     </div>
   )
 }
