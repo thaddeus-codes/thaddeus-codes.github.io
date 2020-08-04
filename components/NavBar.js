bdf7eceee692ecec736346011c0bf6eb56339f8b
Thaddeus Metz <temetz90@gmail.com>
2020-08-04 15:25:57 -0500

    responsive design changes

--- components/NavBar.js
+++ components/NavBar.js
@@ -6,12 +6,25 @@ export default function NavBar() {
   return (
     <Navbar collapseOnSelect expand="lg" id="navbar">
       <Navbar.Brand href="/">Home</Navbar.Brand>
-      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+      <Navbar.Toggle aria-controls="responsive-navbar-nav">
+        <img
+          src="hamburger.png"
+          alt="hamburger icon for nav bar"
+          height="100%"
+          width="auto"
+        />
+      </Navbar.Toggle>
       <Navbar.Collapse id="responsive-navbar-nav">
         <Nav className="ml-auto">
-          <Nav.Link href="#about-me" to='#about-me'>About Me</Nav.Link>
-          <Nav.Link href="#projects" to='#projects'>Projects</Nav.Link>
-          <Nav.Link href="#contact-me" to='#contact-me'>Contact Me</Nav.Link>
+          <Nav.Link href="#about-me" to="#about-me">
+            About Me
+          </Nav.Link>
+          <Nav.Link href="#projects" to="#projects">
+            Projects
+          </Nav.Link>
+          <Nav.Link href="#contact-me" to="#contact-me">
+            Contact Me
+          </Nav.Link>
         </Nav>
       </Navbar.Collapse>
     </Navbar>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import React from "react"
+import { Helmet } from "react-helmet"
 import NavBar from "../../components/NavBar"
 import ContactMe from "../../components/ContactMe"
 import AboutMe from "../../components/AboutMe"
@@ -10,6 +11,9 @@ import "../style.css"
 export default function Home() {
   return (
     <div>
+      <Helmet>
+        <title>Thaddeus Codes</title>
+      </Helmet>
       <div id="header">
         <div id="header-text">
           <h1>Let's get started!</h1>
