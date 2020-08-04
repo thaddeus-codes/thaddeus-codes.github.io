b75b5dde3d7c923a1ba22d27e3b051419dd752c7
Thaddeus Metz <temetz90@gmail.com>
2020-08-04 17:41:54 -0500

    animated hamburger menu

--- components/NavBar.js
+++ components/NavBar.js
@@ -3,16 +3,29 @@ import Navbar from "react-bootstrap/Navbar"
 import Nav from "react-bootstrap/Nav"
 
 export default function NavBar() {
+  let menuOpen = false
+  const menuBtn = document.querySelector(".menu-btn")
+
+  const animateBurger = function () {
+    if (!menuOpen && menuBtn) {
+      menuBtn.classList.add("open")
+      menuOpen = true
+    } else if (menuOpen && menuBtn) {
+      menuBtn.classList.remove("open")
+      menuOpen = false
+    }
+  }
+
   return (
     <Navbar collapseOnSelect expand="lg" id="navbar">
       <Navbar.Brand href="/">Home</Navbar.Brand>
-      <Navbar.Toggle aria-controls="responsive-navbar-nav">
-        <img
-          src="hamburger.png"
-          alt="hamburger icon for nav bar"
-          height="100%"
-          width="auto"
-        />
+      <Navbar.Toggle
+        aria-controls="responsive-navbar-nav"
+        onClick={animateBurger}
+      >
+        <div className="menu-btn">
+          <div className="menu-btn__burger"></div>
+        </div>
       </Navbar.Toggle>
       <Navbar.Collapse id="responsive-navbar-nav">
         <Nav className="ml-auto">
